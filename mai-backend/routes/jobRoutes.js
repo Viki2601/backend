ae2dafd866d667ccb6decd13a87db083f7e5df84
Vishnu Muthukumar <mcvicky2601@gmail.com>
2025-04-08 08:59:13 +0530

    CompleteCRUD

--- mai-backend/routes/jobRoutes.js
+++ mai-backend/routes/jobRoutes.js
@@ -2,14 +2,45 @@ const express = require('express');
 const router = express.Router();
 const Job = require('../models/Job');
 
+// Posting Jobs
 router.post('/jobs', async (req, res) => {
-    console.log(req.body)
     try {
         const newJob = new Job(req.body);
         await newJob.save();
         res.status(201).json({ message: "Job Posted Successfully", Job: newJob });
     } catch (error) {
         res.status(500).json({ error: "Failed to post job", details: error.message });
+    }
+});
+
+// Get All Jobs
+router.get('/', async (req, res) => {
+    try {
+        const jobs = await Job.find();
+        res.json(jobs);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Update Job Posting
+router.put('/update/:id', async (req, res) => {
+    try {
+        const updatedJob = await Job.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        res.json(updatedJob);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Delete Job Posting
+router.delete('/delete/:id', async (req, res) => {
+    try {
+        await Job.findByIdAndDelete(req.params.id);
+        console.log("first")
+        res.json({ message: "Job Deleted Successfully" });
+    } catch (err) {
+        res.status(500).json(err);
     }
 });
 
